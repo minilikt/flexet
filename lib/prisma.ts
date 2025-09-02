84c5a09f014f0a82fc77e0f90d54cc01f9bc0d7b
Zeabraham <154699408+Zeabraham@users.noreply.github.com>
2025-09-02 11:51:58 +0300

    postinstall and lib

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  global.prisma ||
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "info", "warn", "error"],
+    log: ['query'],
   });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
